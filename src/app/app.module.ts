3d731955711fb22dc11ed8f69d6a9928d41cecaf
nikola.malinovic <nikolamalinovic97@gmail.com>
2020-07-07 19:34:43 +0200

    Add template skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,16 @@ import { MatButtonModule } from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
