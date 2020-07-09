f6ad39f2abd52995d15a5d767abf102b00bf2d57
nikola.malinovic <nikolamalinovic97@gmail.com>
2020-07-09 17:54:54 +0200

    Add autcomplete to location input

--- src/app/weather/weather-dialog/weather-dialog.component.ts
+++ src/app/weather/weather-dialog/weather-dialog.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+/// <reference types="@types/googlemaps" />
+import { Component, OnInit, ViewChild, ElementRef, NgZone } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { MapsAPILoader } from '@agm/core';
+// import { } from '@types/googlemaps';
 
 @Component({
   selector: 'app-weather-dialog',
@@ -11,7 +14,10 @@ export class WeatherDialogComponent implements OnInit {
 
   locationForm: FormGroup;
 
-  constructor(public dialogRef: MatDialogRef<WeatherDialogComponent>, private formBuilder: FormBuilder) { }
+  @ViewChild('search') public searchElement: ElementRef;
+
+  constructor(public dialogRef: MatDialogRef<WeatherDialogComponent>, private formBuilder: FormBuilder,
+              private mapsAPILoader: MapsAPILoader, private ngZone: NgZone) { }
 
   ngOnInit(): void {
     const LATITUDE = '^(\\+|-)?(?:90(?:(?:\\.0{1,6})?)|(?:[0-9]|[1-8][0-9])(?:(?:\\.[0-9]{1,6})?))$';
@@ -21,10 +27,28 @@ export class WeatherDialogComponent implements OnInit {
       latitude: ['', [Validators.required, Validators.pattern(LATITUDE)]],
       longitude: ['', [Validators.required, Validators.pattern(LONGITUDE)]]
     });
+
+    this.places();
   }
 
   onSubmit(): any {
     this.dialogRef.close(this.locationForm.value);
+  }
+
+  places(): any {
+    this.mapsAPILoader.load().then(() => {
+      const autocomplete = new google.maps.places.Autocomplete(this.searchElement.nativeElement, { types: ['address'] });
+
+      autocomplete.addListener('place_changed', () => {
+        this.ngZone.run(() => {
+          const place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+        });
+      });
+    });
   }
 
 }
