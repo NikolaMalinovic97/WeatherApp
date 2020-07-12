b3039d2f6eba4409fa386a4156c0cfa412cae5ff
NikolaMalinovic97 <nikolamalinovic97@gmail.com>
2020-07-12 21:38:10 +0200

    Fix - autocomplete predictions show on dialog init

--- src/app/weather/weather-dialog/weather-dialog.component.ts
+++ src/app/weather/weather-dialog/weather-dialog.component.ts
@@ -25,7 +25,6 @@ export class WeatherDialogComponent implements OnInit {
 
   ngOnInit(): void {
     this.locationForm = this.createLocationForm();
-    this.updatePredictionOptions();
   }
 
   onLocationKeyUp(): any {
